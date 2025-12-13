8548f7d61cb5e01790edb8f5f11c7e80383f8c88
Harsh Bhardwaj <162420522+HARSHBHARDWAJ07@users.noreply.github.com>
2025-12-14 02:26:37 +0530

    Change illustration format from SVG to JPG

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import axios from "axios";
 import {useNavigate} from 'react-router-dom';
 import './CSS/Signin.css';
-import Illustation from '../Assets/ils.svg';
+import Illustation from '../Assets/ils.jpg';
 import { Link } from 'react-router-dom';
 
 const API_URL = process.env.REACT_APP_API_URL;
