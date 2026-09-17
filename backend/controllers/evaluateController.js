c3c2a15274dba027b47147c2dd98298f26361601
Harsh Bhardwaj <harshbhar836@gmail.com>
2026-09-17 18:10:11 +0530

    env final

--- backend/controllers/evaluateController.js
+++ backend/controllers/evaluateController.js
@@ -7,11 +7,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const { Pool } = pkg; 
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -36,20 +36,14 @@ app.use(cors({
 app.set('trust proxy', 1)
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 
 const pool = new pg.Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 
